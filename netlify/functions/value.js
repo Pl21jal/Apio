4cfc79fec35b890438763277d91bc0653cd60213
Pl21jal <reonzabusa@gmail.com>
2025-08-10 14:46:07 +0700

    Update value.js

--- netlify/functions/value.js
+++ netlify/functions/value.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "../../data.json");
+const filePath = path.join(__dirname, "data.json");
 
 exports.handler = async (event) => {
   if (event.httpMethod === "GET") {
